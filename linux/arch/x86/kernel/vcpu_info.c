ab9a1b6cd3401451dc80c20a264e369c1079068b
sgFlags <wb2331@columbia.edu>
2018-02-01 14:22:18 -0500

    hyperhall try

--- linux/arch/x86/kernel/vcpu_info.c
+++ linux/arch/x86/kernel/vcpu_info.c
@@ -3,13 +3,13 @@
 #include <asm/kvm_guest.h>
 #include <asm/syscalls.h>
 
-/*int vcpu_info(int vcpu_id)
+int vcpu_info(int vcpu_id)
 {       
         int ret = 0;
         
         ret = kvm_hypercall1(KVM_HC_X86_VCPU_INFO, vcpu_id);
         return ret;
-}*/
+}
 
 asmlinkage int sys_vcpu_info(void)
 {
@@ -17,5 +17,6 @@ asmlinkage int sys_vcpu_info(void)
 
 	printk("syscall succeed222!!\n");
 	printk("where am I?\n");
+	ret = vcpu_info(2);
 	return ret;	
 }
